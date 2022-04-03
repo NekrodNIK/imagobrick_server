66942f327cfe7c4069a31a0baed14063e1ce158c
Nekrod <NP17210@yandex.ru>
2022-04-03 15:20:10 +0500

    init

--- image_manipulate.py
+++ image_manipulate.py
@@ -1,6 +1,6 @@
 from enum import IntEnum
 
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps
 
 
 class MethodsQuantize(IntEnum):
@@ -8,16 +8,26 @@ class MethodsQuantize(IntEnum):
     MAX_COVERAGE = 1
 
 
+def apply_modifiers_to_image(image: Image,
+                             monochrome: bool = False,
+                             invert: bool = False,
+                             contrast: float = 1) -> Image:
+    image = ImageEnhance.Contrast(image).enhance(contrast)
+
+    if monochrome:
+        image = image.convert("L")
+
+    if invert:
+        image = ImageOps.invert(image)
+
+
 def gen_bricked_image(image: Image,
                       pixel_resolution: int = 40,
                       color_count: int = 5,
-                      method_quantize: MethodsQuantize.value = MethodsQuantize.MEDIAN_CUT) -> Image:
+                      method_quantize=MethodsQuantize.MEDIAN_CUT) -> Image:
     image_man = image.resize((pixel_resolution, pixel_resolution), resample=Image.ADAPTIVE)
 
-    image_man = \
-        ImageEnhance.Contrast(image_man).enhance(pixel_resolution)
-
-    image_man = image_man.quantize(colors=color_count)
+    image_man = image_man.quantize(colors=color_count, method=method_quantize)
 
     image_man = image_man.resize(image.size, resample=Image.NEAREST)
 
