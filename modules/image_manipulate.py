e47a96ce531cb8179ca9a2f5a30d2b2caf7064dc
Nekrod <NP17210@yandex.ru>
2022-04-03 18:53:42 +0500

    some changes

--- modules/image_manipulate.py
+++ modules/image_manipulate.py
@@ -1,6 +1,6 @@
 from enum import IntEnum
 
-from PIL import Image, ImageEnhance, ImageOps
+from PIL import ImageEnhance, ImageOps, Image
 
 
 class MethodsQuantize(IntEnum):
@@ -8,10 +8,10 @@ class MethodsQuantize(IntEnum):
     MAX_COVERAGE = 1
 
 
-def apply_modifiers_to_image(image: Image,
+def apply_modifiers_to_image(image: Image.Image,
                              monochrome: bool = False,
                              invert: bool = False,
-                             contrast: float = 1) -> Image:
+                             contrast: float = 1) -> Image.Image:
     image = ImageEnhance.Contrast(image).enhance(contrast)
 
     if monochrome:
@@ -20,11 +20,13 @@ def apply_modifiers_to_image(image: Image,
     if invert:
         image = ImageOps.invert(image)
 
+    return image
 
-def gen_bricked_image(image: Image,
+
+def gen_bricked_image(image: Image.Image,
                       pixel_resolution: int = 40,
                       color_count: int = 5,
-                      method_quantize=MethodsQuantize.MEDIAN_CUT) -> Image:
+                      method_quantize=MethodsQuantize.MEDIAN_CUT) -> Image.Image:
     image_man = image.resize((pixel_resolution, pixel_resolution), resample=Image.ADAPTIVE)
 
     image_man = image_man.quantize(colors=color_count, method=method_quantize)
--- server.py
+++ server.py
@@ -2,13 +2,13 @@ from PIL import Image
 from fastapi import FastAPI
 
 from modules.image_manipulate import gen_bricked_image
-from modules.models import GenModel, ImageModel
+from modules.models import RequestModel, ResponseModel, ImageModel
 
 app = FastAPI()
 
 
-@app.post("/gen/image")
-def gen_image(request: GenModel):
+@app.post("/gen/image", response_model=ResponseModel)
+def gen_image(request: RequestModel):
     generated_image = gen_bricked_image(
         Image.frombytes(data=request.image.data,
                         size=(request.image.width,
@@ -16,10 +16,9 @@ def gen_image(request: GenModel):
                         mode="RGB"),
     )
 
-    response = GenModel(image=ImageModel(
-        data=generated_image.tobytes(),
-        width=request.image.width,
-        height=request.image.height
-    ))
+    response = ResponseModel()
+    response.image = ImageModel(data=generated_image.tobytes(),
+                                width=generated_image.width,
+                                height=generated_image.height)
     return response
 
